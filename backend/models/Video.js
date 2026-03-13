75dcefd45eb62fed5f9974e17d50962ff25ad32f
rudysetiawan111 <rudysetiawan111@gmail.com>
2026-03-13 18:35:56 +0700

    Update Video.js

--- backend/models/Video.js
+++ backend/models/Video.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 
+const VideoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  platform: { type: String, enum: ['YouTube', 'TikTok'], required: true },
+  channel: { type: mongoose.Schema.Types.ObjectId, ref: 'Channel' },
+  views: Number,
+  likes: Number,
+  uploadDate: Date,
+  url: String
+});
+
+module.exports = mongoose.model('Video', VideoSchema);
