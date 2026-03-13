e0cc5f10ed0adcc4bd50f9d8d72a0ffc3ae304f3
rudysetiawan111 <rudysetiawan111@gmail.com>
2026-03-13 18:34:29 +0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: String,
+  platform: { type: String, enum: ['YouTube', 'TikTok'], required: true },
+  apiKey: String, // bisa simpan API key / token
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
