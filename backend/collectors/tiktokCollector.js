916ceb6d5a42d819264865c8e70e9c19e1c74dc4
rudysetiawan111 <rudysetiawan111@gmail.com>
2026-03-13 18:28:14 +0700

    Update tiktokCollector.js

--- backend/collectors/tiktokCollector.js
+++ backend/collectors/tiktokCollector.js
@@ -1,1 +1,9 @@
+const { getUserProfile, getVideos } = require('../services/tiktokService');
 
+async function fetchTikTokData() {
+  const profile = await getUserProfile();
+  const videos = await getVideos();
+  return { profile, videos };
+}
+
+module.exports = { fetchTikTokData };
