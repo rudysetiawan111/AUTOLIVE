29d3a3e94d458d05f7a9a87654ece9d37d3dc4ce
rudysetiawan111 <rudysetiawan111@gmail.com>
2026-03-13 18:27:38 +0700

    Update youtubeCollector.js

--- backend/collectors/youtubeCollector.js
+++ backend/collectors/youtubeCollector.js
@@ -1,1 +1,8 @@
+const { getVideos } = require('../services/youtubeService');
 
+async function fetchYouTubeVideos() {
+  const videos = await getVideos();
+  return videos;
+}
+
+module.exports = { fetchYouTubeVideos };
