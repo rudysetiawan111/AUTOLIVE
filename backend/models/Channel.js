d4eedeabc4040edd88ec3d54caab4573253cebed
rudysetiawan111 <rudysetiawan111@gmail.com>
2026-03-13 18:35:14 +0700

    Update Channel.js

--- backend/models/Channel.js
+++ backend/models/Channel.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const ChannelSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  platform: { type: String, enum: ['YouTube', 'TikTok'], required: true },
+  subscriberCount: Number,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Channel', ChannelSchema);
