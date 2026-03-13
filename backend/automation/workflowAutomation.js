9c54b12a2ef6dc4cb0d6722266dfaf1348ab3f1c
rudysetiawan111 <rudysetiawan111@gmail.com>
2026-03-13 18:31:47 +0700

    Update workflowAutomation.js

--- backend/automation/workflowAutomation.js
+++ backend/automation/workflowAutomation.js
@@ -1,1 +1,22 @@
+const { fetchYouTubeVideos } = require('../collectors/youtubeCollector');
+const { fetchTikTokData } = require('../collectors/tiktokCollector');
+const { calculateEngagement } = require('../analyzers/engagementAnalyzer');
 
+async function runWorkflow() {
+  console.log('--- Running Workflow ---');
+
+  const ytVideos = await fetchYouTubeVideos();
+  const ttData = await fetchTikTokData();
+
+  ytVideos.forEach(v => {
+    console.log(`[YouTube] ${v.title} Engagement: ${calculateEngagement(v)}`);
+  });
+
+  ttData.videos.forEach(v => {
+    console.log(`[TikTok] ${v.title} Engagement: ${calculateEngagement(v)}`);
+  });
+
+  console.log('--- Workflow Finished ---');
+}
+
+module.exports = { runWorkflow };
