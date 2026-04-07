e13fc6097a6d5f3f2d8c3ffd773866a25a28f151
rudysetiawan111 <rudysetiawan111@gmail.com>
2026-04-07 18:13:13 +0700

    Create redis.ts

---
+++ lib/redis.ts
@@ -0,0 +1,7 @@
+import IORedis from 'ioredis';
+
+export const connection = new IORedis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
+});
